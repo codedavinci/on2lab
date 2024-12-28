8ab9ab6dc6b5e9a646eaf21109d7b48bbf4c8f7e
Eddie Silva <codedavinci.now@gmail.com>
2024-12-27 23:35:27 -0500

    Updated header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Schedule from "./pages/Schedule";
@@ -8,17 +8,7 @@ export default function App() {
   return (
     <Router>
       <div className="p-4">
-        <nav className="mb-4">
-          <a href="/" className="mr-4 text-blue-500">
-            Home
-          </a>
-          <a href="/about" className="mr-4 text-blue-500">
-            About
-          </a>
-          <a href="/contact" className="text-blue-500">
-            Contact
-          </a>
-        </nav>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<Products />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./output.css";
+
 import App from "./App.tsx";
+import "./input.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
